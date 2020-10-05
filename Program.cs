612b0cf82c6cd0dc3c0d5d101c3c18ee531310e2
Anton Firszov <antonfir@gmail.com>
2020-10-05 02:06:52 +0200

    ImageSharpTest

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace ImageSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inPath = "../../../input.jpg";
+            
+            using Image image = Image.Load(inPath);
+            image.Mutate(c => c.Resize(c.GetCurrentSize()/2));
+            image.Save("./output.jpg");
+        }
+    }
+}
